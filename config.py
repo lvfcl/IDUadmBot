95f6ee492d3b1cc4c8e692f8edc57bae43e9e550
lvfcl <83322580+lvfcl@users.noreply.github.com>
2025-07-05 07:40:21 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,29 @@
+
+CHANNEL_ID = -0123456789
+
+
+
+studrada_tg_link = "https://t.me/+PAqNTnpJbG1kNDZi"
+
+coefficients_link = "https://start.karazin.ua/fakultety/ipa/programi/25"
+
+NMT_link = "https://osvita.ua/consultations/konkurs-ball"
+
+admins_password = "0123456789"
+
+link_schedule = "https://www.google.com"
+
+link_info = "https://forms.gle/6y1EhRqZQmQysodJ6"
+
+link_social = """                                                                    
+Слідкуйте за нами в Інстаграм:
+https://www.instagram.com/derzhuprav.karazina?igsh=MWM3MzEzOWo1MWNwZQ==
+
+А також майте завжди доступ до актуальних новин в нашому Телеграм каналі:
+https://t.me/karazinipa1995
+"""
+               
+link_consultation = """
+Вітаю, мене звати Максим, можете звертатися до мене у разі виникнення питань @Maksym_Serhiienko
+"""
+
---
+++ configC.py
@@ -0,0 +1,7 @@
+
+BOT_TOKEN = "0123456789:AAAAAAA-BBBBBBH_eCCCCCC8"
+
+MEDIA_PATH = ".\\config_media\\"
+
+ARCHIVE_FOLDER = ".\\archive"
+
